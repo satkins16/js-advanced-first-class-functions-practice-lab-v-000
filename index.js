d0ea27b4cdb8406178e216076a8cd510f99c890e
Scott Atkins <satkins16@gmail.com>
2017-11-09 00:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const logDriverNames = function (drivers) {
     console.log(driver.name)
   })
 }
+
+const logDriversByHometown = function (drivers, location) {
+  drivers.forEach(function (driver) {
+    console.log()
+  })
+}
